ded4f4a68aaec782fd456ed5bb3df377eb400385
Thomas-Christie <thomashamish@hotmail.com>
2021-12-01 18:50:28 +0000

    Added ability to choose your own features for model

--- housing_fynesse/address.py
+++ housing_fynesse/address.py
@@ -24,12 +24,7 @@ import scipy.stats"""
 """Address a particular question that arises from the data"""
 
 
-def predict_price_without_distance(conn, latitude, longitude, year, property_type):
-    features = {"amenity": ["cafe", "restaurant", "school", "college", "bar"],
-                "public_transport": [],
-                "shop": [],
-                "leisure": ["park"]
-                }
+def predict_price_without_distance(conn, latitude, longitude, year, property_type, features):
     box_width = 4
     box_height = 4
     distance_from_house = 1
@@ -56,8 +51,7 @@ def predict_price_without_distance(conn, latitude, longitude, year, property_typ
     column_names.append("constant")
     design = train[column_names]
     y = train['price']
-    m_linear_basis = sm.OLS(y, design)
-    # m_linear_basis = sm.GLM(y, design, family=sm.families.Gaussian(link=sm.families.links.log))
+    m_linear_basis = sm.GLM(y, design, family=sm.families.Gaussian(link=sm.families.links.log))
     results_basis = m_linear_basis.fit()
     test_features = test[column_names]
     results = results_basis.get_prediction(test_features).summary_frame(alpha=0.05)['mean']
@@ -67,7 +61,6 @@ def predict_price_without_distance(conn, latitude, longitude, year, property_typ
     prediction_features['constant'] = 1
     prediction_features = prediction_features[column_names]
     predicted_price = results_basis.get_prediction(prediction_features).summary_frame(alpha=0.05)['mean']
-    print("Predicted Price is: ", predicted_price)
     print(results_basis.summary())
     plt.rcParams["figure.figsize"] = (20, 10)
     plt.scatter(test['price'], test['prediction'])
@@ -83,22 +76,16 @@ def predict_price_without_distance(conn, latitude, longitude, year, property_typ
     ax.set_aspect('equal')
     ax.set_xlim(lims)
     ax.set_ylim(lims)
+    print(test.to_string())
     rmse = mean_squared_error(test['price'], test['prediction'], squared=False)
     mape = mean_absolute_percentage_error(test['price'], test['prediction'])
     print("Root Mean Squared Error: ", rmse)
     print("Mean Absolute Percentage Error: ", mape)
-    return test
+    print("Predicted Price: ", predicted_price)
+    return predicted_price
 
 
-def predict_price_with_distance(conn, latitude, longitude, year, property_type):
-    features = {"amenity": ["cafe", "restaurant", "school", "college", "bar"],
-                "public_transport": [],
-                "shop": [],
-                "leisure": ["park"]}
-    distance_features = {"amenity": ["cafe", "restaurant", "school", "college", "bar"],
-                         "public_transport": [],
-                         "shop": [],
-                         "leisure": ["park"]}
+def predict_price_with_distance(conn, latitude, longitude, year, property_type, features, distance_features):
     column_names = []
     distance_column_names = []
     box_width = 4
@@ -137,14 +124,12 @@ def predict_price_with_distance(conn, latitude, longitude, year, property_type):
     test_features = test[feature_cols]
     results = results_basis.get_prediction(test_features).summary_frame(alpha=0.05)['mean']
     test['prediction'] = results
-
     prediction_features = assess.get_test_features(longitude, latitude, box_width, box_height, distance_from_house,
                                                    year,
                                                    features, distance_features)
     prediction_features['constant'] = 1
     prediction_features = prediction_features[feature_cols]
     predicted_price = results_basis.get_prediction(prediction_features).summary_frame(alpha=0.05)['mean']
-    print("Predicted Price: ", predicted_price)
     prediction_features['pred'] = predicted_price
     print(results_basis.summary())
     plt.rcParams["figure.figsize"] = (20, 10)
@@ -161,8 +146,10 @@ def predict_price_with_distance(conn, latitude, longitude, year, property_type):
     ax.set_aspect('equal')
     ax.set_xlim(lims)
     ax.set_ylim(lims)
+    print(test.to_string())
     rmse = mean_squared_error(test['price'], test['prediction'], squared=False)
     mape = mean_absolute_percentage_error(test['price'], test['prediction'])
     print("Root Mean Squared Error: ", rmse)
     print("Mean Absolute Percentage Error: ", mape)
-    return test
+    print("Predicted Price: ", predicted_price)
+    return predicted_price
